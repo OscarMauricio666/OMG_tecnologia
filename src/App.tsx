422aa9f67b3a3928ab4708dce38acb2a9b518878
OscarMauricio666 <mauriciorondonfajardo@gmail.com>
2023-07-27 07:25:22 -0500

    validando render1

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>OscarMauricio6660000666</h1>
+      <h1>OscarMauricio666000066690</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
