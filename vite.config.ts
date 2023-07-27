cd3dbcf13e685b2a04a3ef680d85cbac8b88a56e
OscarMauricio666 <mauriciorondonfajardo@gmail.com>
2023-07-27 06:41:53 -0500

    validndo el dominio0

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'omgtecnologia.com'
+  base: 'oscarmauricio666.github.io'
 })
